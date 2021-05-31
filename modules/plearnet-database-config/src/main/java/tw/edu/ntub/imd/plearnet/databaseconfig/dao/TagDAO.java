e135a4efb8811524ee68f28235392cf3a22899d7
fuuwuu <cvn319086@gmail.com>
2021-05-31 20:38:38 +0800

    create TagDAO

---
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/TagDAO.java
@@ -0,0 +1,9 @@
+package tw.edu.ntub.imd.plearnet.databaseconfig.dao;
+
+import org.springframework.stereotype.Repository;
+import tw.edu.ntub.imd.plearnet.databaseconfig.entity.Tag;
+
+@Repository
+public interface TagDAO extends BaseDAO<Tag, String>{
+
+}
