23f28a5faf2c7eeb3c53a6032590cc047275f0de
fuuwuu <cvn319086@gmail.com>
2021-06-05 15:15:57 +0800

    create Topic Bean

---
+++ src/main/java/tw/edu/ntub/imd/plearnet/bean/TopicBean.java
@@ -0,0 +1,17 @@
+package tw.edu.ntub.imd.plearnet.bean;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TopicBean {
+    private Integer id;
+    private String title;
+    private String content;
+    private float view;
+    private float likes;
+    private LocalDateTime create_date;
+    private LocalDateTime edit_date;
+    private Integer tag_id;
+}
