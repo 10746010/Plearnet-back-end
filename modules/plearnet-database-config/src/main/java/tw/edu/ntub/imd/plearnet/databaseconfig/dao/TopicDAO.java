42e4bd9cf9facbbee94effd451c47a493611f02a
fuuwuu <cvn319086@gmail.com>
2021-06-05 15:14:02 +0800

    create TopicDAO

---
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/TopicDAO.java
@@ -0,0 +1,9 @@
+package tw.edu.ntub.imd.plearnet.databaseconfig.dao;
+
+import org.springframework.stereotype.Repository;
+import tw.edu.ntub.imd.plearnet.databaseconfig.entity.Topic;
+
+@Repository
+public interface TopicDAO extends BaseDAO<Topic, String>{
+
+}
