d678343fa24bc09f7e02b6a46f0eece6e194df84
fuuwuu <cvn319086@gmail.com>
2021-06-05 15:17:15 +0800

    update UserAccount Bean(new name,sex,registered)

--- src/main/java/tw/edu/ntub/imd/plearnet/bean/UserAccountBean.java
+++ src/main/java/tw/edu/ntub/imd/plearnet/bean/UserAccountBean.java
@@ -2,10 +2,15 @@ package tw.edu.ntub.imd.plearnet.bean;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class UserAccountBean {
     private String account;
     private String password;
     private String email;
     private Integer id;
+    private String name;
+    private String sex;
+    private LocalDateTime register;
 }
