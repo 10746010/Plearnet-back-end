5ac6a4009e26d12acf29a315728021561471423d
fuuwuu <cvn319086@gmail.com>
2021-06-29 13:08:45 +0800

    update TopicService

--- src/main/java/tw/edu/ntub/imd/plearnet/service/TopicService.java
+++ src/main/java/tw/edu/ntub/imd/plearnet/service/TopicService.java
@@ -1,2 +1,7 @@
-package tw.edu.ntub.imd.plearnet.service;public interface TopicService {
+package tw.edu.ntub.imd.plearnet.service;
+
+import tw.edu.ntub.imd.plearnet.bean.TopicBean;
+
+public interface TopicService extends BaseService<TopicBean, String>{
+
 }
