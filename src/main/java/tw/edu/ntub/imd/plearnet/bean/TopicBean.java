1d7532ee87cf8ea7a71b273cc288269c0d659e47
fuuwuu <cvn319086@gmail.com>
2021-07-02 23:35:55 +0800

    update validation in TopicBean

--- src/main/java/tw/edu/ntub/imd/plearnet/bean/TopicBean.java
+++ src/main/java/tw/edu/ntub/imd/plearnet/bean/TopicBean.java
@@ -2,16 +2,24 @@ package tw.edu.ntub.imd.plearnet.bean;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 @Data
 public class TopicBean {
+    @Null(message = "id - 不得填寫")
     private Integer id;
+    @NotBlank(message = "標題 - 未填寫")
     private String title;
+    @NotBlank(message = "筆記內容 - 未填寫")
+    @Size(max = 200, message = "筆記內容 - 輸入字數大於{max}個字")
     private String content;
     private float view;
     private float likes;
+    @Null(message = "開始時間 - 不得填寫")
     private LocalDateTime createDate;
+    @Null(message = "最後修改時間 - 不得填寫")
     private LocalDateTime editDate;
+    @NotNull(message = "筆記分類 - 未填寫")
     private Integer tagId;
 }
