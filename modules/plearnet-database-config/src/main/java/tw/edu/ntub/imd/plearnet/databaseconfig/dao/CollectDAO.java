d1aa295e9df80211a2766230c041b246d5f959d9
fuuwuu <cvn319086@gmail.com>
2021-07-07 13:20:02 +0800

    create Collect listener,entity,DAO and bean

---
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/CollectDAO.java
@@ -0,0 +1,9 @@
+package tw.edu.ntub.imd.plearnet.databaseconfig.dao;
+
+import org.springframework.stereotype.Repository;
+import tw.edu.ntub.imd.plearnet.databaseconfig.entity.Collect;
+
+@Repository
+public interface CollectDAO extends BaseDAO<Collect,String>{
+
+}
---
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/entity/listener/CollectListener.java
@@ -0,0 +1,4 @@
+package tw.edu.ntub.imd.plearnet.databaseconfig.entity.listener;
+
+public class CollectListener {
+}
