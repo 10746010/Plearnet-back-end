295522288087e77cd940e47949b76630b05b9096
fuuwuu <cvn319086@gmail.com>
2021-07-15 15:51:12 +0800

    update findByTagType function

--- modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/TagDAO.java
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/TagDAO.java
@@ -3,7 +3,9 @@ package tw.edu.ntub.imd.plearnet.databaseconfig.dao;
 import org.springframework.stereotype.Repository;
 import tw.edu.ntub.imd.plearnet.databaseconfig.entity.Tag;
 
+import java.util.List;
+
 @Repository
 public interface TagDAO extends BaseDAO<Tag, String>{
-
+    List<Tag> findByTagType(Integer tagType);
 }
