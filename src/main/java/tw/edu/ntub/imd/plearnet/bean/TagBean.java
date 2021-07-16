41ae5a1e233becfcb26e26d0fa79a22d98822b26
fuuwuu <cvn319086@gmail.com>
2021-07-16 14:20:36 +0800

    update valid in TagBean

--- src/main/java/tw/edu/ntub/imd/plearnet/bean/TagBean.java
+++ src/main/java/tw/edu/ntub/imd/plearnet/bean/TagBean.java
@@ -2,9 +2,14 @@ package tw.edu.ntub.imd.plearnet.bean;
 
 import lombok.Data;
 
+import javax.validation.constraints.*;
+
 @Data
 public class TagBean {
+    @Null(message = "id - 不得填寫")
     private Integer id;
+    @NotBlank(message = "tag名稱 - 未填寫")
     private String name;
+    @NotNull(message = "tag類別 - 未填寫")
     private Integer tagType;
 }
