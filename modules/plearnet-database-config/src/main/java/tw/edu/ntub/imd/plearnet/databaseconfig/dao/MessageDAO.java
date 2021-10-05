58e5d7bdd31dc20fe6ee1735c220bbde834470d5
fuuwuu <cvn319086@gmail.com>
2021-10-05 14:57:18 +0800

    update findByTopicId function

--- modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/MessageDAO.java
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/MessageDAO.java
@@ -3,7 +3,9 @@ package tw.edu.ntub.imd.plearnet.databaseconfig.dao;
 import org.springframework.stereotype.Repository;
 import tw.edu.ntub.imd.plearnet.databaseconfig.entity.Message;
 
+import java.util.List;
+
 @Repository
 public interface MessageDAO extends BaseDAO<Message, String>{
-
+    List<Message> findByTopicId(Integer topicID);
 }
