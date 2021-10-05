8e431427904cf656ea03f6fdde40b21d306f79db
fuuwuu <cvn319086@gmail.com>
2021-10-05 15:01:36 +0800

    update findById function

--- modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/TopicDAO.java
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/dao/TopicDAO.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Repository;
 import tw.edu.ntub.imd.plearnet.databaseconfig.entity.Topic;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TopicDAO extends BaseDAO<Topic, String>{
     List<Topic> findByTagId(Integer tag);
+
+    Optional<Topic> findById(Integer id);
 }
