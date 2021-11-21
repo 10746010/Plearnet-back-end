6567543b9bb878c53c79cdd3040cafa91d65b35e
fuuwuu <cvn319086@gmail.com>
2021-11-22 01:47:08 +0800

    set History 's ViewTime default

--- modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/entity/listener/HistoryListener.java
+++ modules/plearnet-database-config/src/main/java/tw/edu/ntub/imd/plearnet/databaseconfig/entity/listener/HistoryListener.java
@@ -1,4 +1,15 @@
 package tw.edu.ntub.imd.plearnet.databaseconfig.entity.listener;
 
+import tw.edu.ntub.imd.plearnet.databaseconfig.entity.History;
+
+import javax.persistence.PrePersist;
+import java.time.LocalDateTime;
+
 public class HistoryListener {
+    @PrePersist
+    public void preSave(History history) {
+        if(history.getViewTime() == null){
+            history.setViewTime(LocalDateTime.now());
+        }
+    }
 }
